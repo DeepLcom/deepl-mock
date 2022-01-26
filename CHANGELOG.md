46f351df06e6fae1afee3b5691261f77f23db07f
Daniel Jones <daniel.jones@deepl.com>
2022-01-26 20:22:32 +0100

    Increase request body size limits

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Deprecated
 ### Removed
 ### Fixed
+* Raise request body size limits to 50 MiB.
 ### Security
 
 
--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const express = require('express');
 const httpProxy = require('http-proxy');
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 const nocache = require('nocache');
 
